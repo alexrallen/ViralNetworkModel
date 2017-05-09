2eb2bdb6e886b6f37d153285f0cc5e5b6ca412d4
Charles Goodman <goodman17c@ncssm.edu>
2017-05-09 09:09:41 -0400

    Added Pseudocode

--- ViralModel.py
+++ ViralModel.py
@@ -1,3 +1,13 @@
 #Abstract class for a viral model
 #Will contain calculations that occur between nodes
 #Needs to be structure-agnostic
+
+#SI model
+#The suseptible vertice will count the number of infected neighbors.
+#Some constant times the number of infected neighbors is the probability of changing state from S to I.
+
+
+#SIR model
+#The suseptible vertices will count the number of infected neighbors.
+#Some constant times the number of infected neighbors is the probability of changing state from S to I.
+#Same process for I to R
