d3ef4dd35ae332b331037bd8f6440dfc7f1010f9
Forrest Hurley <Forrest Hurley>
2017-05-18 08:19:59 -0400

    Made code work

--- Examples.py
+++ Examples.py
@@ -1,21 +1,25 @@
-mport matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import networkx as nx
+import random
 
-G=nx.erdos_renyi_graph(10,0.15)
+G=nx.erdos_renyi_graph(200,0.02)
 
 nx.set_node_attributes(G,'infected',False)
 
 G.node[0]['infected']=True
 
-for node in G.nodes_iter(data=False):
-	print(G.node[node]['infected'])
+#for node in G.nodes_iter(data=False):
+#	print(G.node[node]['infected'])
 
-for i in range(10):
+for i in range(60):
 	numInfected=0
 	for node in G.nodes_iter(data=False):
-		if (G.node[node]['infected']==False)
-			numInfectedNeighbors = 0
+		if (G.node[node]['infected']==False):
 			for neighbor in nx.all_neighbors(G,node):
 				if (G.node[neighbor]['infected']==True):
-					numInfectedNeighbors=numInfectedNeighbors+1
-			print(numInfectedNeighbors)
+					if(random.random()<0.1):
+						G.node[node]['infected']=True
+	for node in G.nodes_iter(data=False):
+		if(G.node[node]['infected']==True):
+			numInfected += 1
+	print(numInfected)
