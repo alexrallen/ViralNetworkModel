4b430ff71db1c5d819053160f2af3f62e6d6fbd7
Alexander Allen <admin@altechcode.com>
2017-05-19 19:37:33 -0400

    Modify stuff

--- Examples.py
+++ Examples.py
@@ -1,4 +1,4 @@
-mport matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import networkx as nx
 
 G=nx.erdos_renyi_graph(10,0.15)
@@ -13,7 +13,7 @@ for node in G.nodes_iter(data=False):
 for i in range(10):
 	numInfected=0
 	for node in G.nodes_iter(data=False):
-		if (G.node[node]['infected']==False)
+		if (G.node[node]['infected']==False):
 			numInfectedNeighbors = 0
 			for neighbor in nx.all_neighbors(G,node):
 				if (G.node[neighbor]['infected']==True):
